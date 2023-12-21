68effc9a4a27bc4678a60cd8c60159f2baeda483
Shreekar Gade <123613407+Shreekar11@users.noreply.github.com>
2023-12-21 16:45:32 +0530

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
     return (
         <nav className="navbar p-5 flex items-center max-w-xl m-auto border-b-2">
-            <Link to="/react-blog" className="logo text-4xl font-semibold">ReactBlog</Link>
+            <Link to="/" className="logo text-4xl font-semibold">ReactBlog</Link>
 
             <div className="links ml-auto flex space-x-8 font-medium">
-                <Link to="/react-blog">Home</Link>    
+                <Link to="/">Home</Link>    
                 <Link to="/create">New Blog</Link>
             </div>
   
